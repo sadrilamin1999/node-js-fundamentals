0ffb322924b8664ff726c737dff04206984e44e3
SadrilAmin1999 <sadrilshuvo420@gmail.com>
2023-02-12 11:00:40 +0600

    file write system added

--- files.js
+++ files.js
@@ -5,3 +5,19 @@ fs.readFile("./texts/text1.txt", (err, data) => {
   if (err) console.log(err);
   console.log(data.toString());
 });
+console.log("Last line");
+
+// write
+/* fs.writeFile("./texts/text1.txt", "Hello none", () => {
+  console.log("File writen successfully");
+}); */
+
+// file create & write
+
+/* fs.writeFile("./texts/text2.txt", "Hello again", () => {
+  console.log("File writen successfully");
+}); */
+
+fs.writeFile("./texts/text3.txt", "Hello file3", () => {
+  console.log("file three successfully red");
+});

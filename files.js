4af414839bf4c6df89d0299f9675fbd0ca6cc109
SadrilAmin1999 <sadrilshuvo420@gmail.com>
2023-02-12 12:21:29 +0600

    folder add or remove system added

--- files.js
+++ files.js
@@ -1,11 +1,11 @@
 const fs = require("fs");
 
 // read
-fs.readFile("./texts/text1.txt", (err, data) => {
+/* fs.readFile("./texts/text1.txt", (err, data) => {
   if (err) console.log(err);
   console.log(data.toString());
 });
-console.log("Last line");
+console.log("Last line"); */
 
 // write
 /* fs.writeFile("./texts/text1.txt", "Hello none", () => {
@@ -18,6 +18,13 @@ console.log("Last line");
   console.log("File writen successfully");
 }); */
 
-fs.writeFile("./texts/text3.txt", "Hello file3", () => {
+/* fs.writeFile("./texts/text3.txt", "Hello file3", () => {
   console.log("file three successfully red");
-});
+}); */
+
+// directory
+if (!fs.existsSync("./node")) {
+  fs.mkdir("./node", (err) => console.log("Folder is created"));
+} else {
+  fs.rmdir("./node", (err) => console.log("Folder is removed"));
+}
